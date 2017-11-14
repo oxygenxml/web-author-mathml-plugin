c5040e2985a86cd14a5d15fb88c29ebd5c41a672
Cristian Talau <cristi_talau@sync.ro>
2017-11-14 14:21:00 +0200

    WA-1599: Added a specific plugin for MathML.

---
+++ src/main/java/com/oxygenxml/sdksamples/mathml/MathMLPlugin.java
@@ -0,0 +1,20 @@
+package com.oxygenxml.sdksamples.mathml;
+
+import ro.sync.exml.plugin.Plugin;
+import ro.sync.exml.plugin.PluginDescriptor;
+
+/**
+ * Plug-in used to render and edit MathML equations.
+ * 
+ * @author costi_dumitrescu
+ */
+public class MathMLPlugin extends Plugin {
+  /**
+   * Constructor.
+   * 
+   * @param descriptor Plugin descriptor.
+   */
+  public MathMLPlugin(PluginDescriptor descriptor) {
+    super(descriptor);
+  }
+}
