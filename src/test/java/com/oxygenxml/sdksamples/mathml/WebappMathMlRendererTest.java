e444b02ca714182cedc7db83fceed362639c69b3
SYNCROSOFT\mihai_coanda <coanda.mihai@gmail.com>
2019-03-27 16:26:26 +0200

    WA-2849 added TC.

--- src/test/java/com/oxygenxml/sdksamples/mathml/WebappMathMlRendererTest.java
+++ src/test/java/com/oxygenxml/sdksamples/mathml/WebappMathMlRendererTest.java
@@ -16,7 +16,13 @@ public class WebappMathMlRendererTest {
     BufferedImage image = new BufferedImage(500, 600, BufferedImage.TYPE_BYTE_BINARY);
     String generatedImgHtml = new WebappMathMLRenderer().generateImgHtml(image, "", 1L, "hash", "<xml_content/>", false);
     
+    
+    System.out.println(generatedImgHtml);
+    
     assertTrue(generatedImgHtml.indexOf("width=\"500\"") != -1);
     assertTrue(generatedImgHtml.indexOf("height=\"600\"") != -1);
+    
+    assertTrue(generatedImgHtml.indexOf("\" alt=\"") != -1);
+    assertTrue(generatedImgHtml.indexOf("data-alt=\"") == -1);
   }
 }
